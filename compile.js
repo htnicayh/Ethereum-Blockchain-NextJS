f9b27c91b7eaf8c9dfaaceb6f5b4f18a49185bb2
htnicayh <hieuhp1306@gmail.com>
2021-11-19 02:16:55 +0700

    compile solidity contract

---
+++ compile.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts[':Inbox']
+
+// Bytecode: deploy to network
+// ABI: move to web3 constructor to test with Mocha
